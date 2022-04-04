aeb798b89e3e8e500a9ee93075fede8c967c97d5
Mattias Gradin <mattias.gradin@apendo.se>
2022-04-04 13:51:08 +0200

    Init project

---
+++ src/main/java/se/sundsvall/processengine/parkingpermit/ParkingPermitApplication.java
@@ -0,0 +1,13 @@
+package se.sundsvall.processengine.parkingpermit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingPermitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingPermitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/sundsvall/processengine/parkingpermit/ParkingPermitApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sundsvall.processengine.parkingpermit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingPermitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
