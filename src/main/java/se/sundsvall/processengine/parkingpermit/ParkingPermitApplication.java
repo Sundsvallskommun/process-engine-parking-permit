e58af4897613cd69febfdb3b98ef58b2daa8db2a
PERSONAL\mat16gra <mattias.gradin@apendo.se>
2022-05-03 10:46:42 +0200

    Fixed Dept44-parent dependency

--- src/main/java/se/sundsvall/processengine/parkingpermit/ParkingPermitApplication.java
+++ src/main/java/se/sundsvall/processengine/parkingpermit/ParkingPermitApplication.java
@@ -2,8 +2,9 @@ package se.sundsvall.processengine.parkingpermit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import se.sundsvall.dept44.ServiceApplication;
 
-@SpringBootApplication
+@ServiceApplication
 public class ParkingPermitApplication {
 
 	public static void main(String[] args) {
