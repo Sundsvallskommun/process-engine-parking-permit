a08e6d5a44d8a8d97f5487b330403e50e33285f7
mattias.gradin@apendo.se <mattias.gradin@apendo.se>
2022-06-01 14:03:53 +0200

    Use Lombok annotation

--- src/main/java/se/sundsvall/processengine/parkingpermit/api/ParkingPermitResponse.java
+++ src/main/java/se/sundsvall/processengine/parkingpermit/api/ParkingPermitResponse.java
@@ -1,13 +1,10 @@
 package se.sundsvall.processengine.parkingpermit.api;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
 public class ParkingPermitResponse {
+    @Schema(example = "bfc9d438-db4e-11ec-bb5b-0242ac110003")
     private String processId;
-
-    public String getProcessId() {
-        return processId;
-    }
-
-    public void setProcessId(String processId) {
-        this.processId = processId;
-    }
 }
