bb945e9ca85e6a309269fc8216f79432f49b95e0
mattias.gradin@apendo.se <mattias.gradin@apendo.se>
2022-06-15 13:02:53 +0200

    New exteral task client

--- src/main/java/se/sundsvall/processengine/parkingpermit/integration/camunda/CaseDataWorker.java
+++ src/main/java/se/sundsvall/processengine/parkingpermit/integration/camunda/CaseDataWorker.java
@@ -1,6 +1,19 @@
 package se.sundsvall.processengine.parkingpermit.integration.camunda;
 
-public class CaseDataWorker {
+import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.springframework.context.annotation.Configuration;
 
-    
+@Configuration
+@ExternalTaskSubscription("caseData")
+public class CaseDataWorker implements ExternalTaskHandler {
+
+    @Override
+    public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+
+        System.out.println("Test test :)");
+        externalTaskService.complete(externalTask);
+    }
 }
