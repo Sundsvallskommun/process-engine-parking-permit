46f22c27f5d39a968b20a15c2a94f10c866a921c
mattias.gradin@apendo.se <mattias.gradin@apendo.se>
2022-06-15 13:51:23 +0200

    First integration to CaseData

--- src/main/java/se/sundsvall/processengine/parkingpermit/integration/casedata/ErrandsClient.java
+++ src/main/java/se/sundsvall/processengine/parkingpermit/integration/casedata/ErrandsClient.java
@@ -1,6 +1,10 @@
 package se.sundsvall.processengine.parkingpermit.integration.casedata;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 public class ErrandsClient {
 
@@ -12,9 +16,25 @@ public class ErrandsClient {
 
     public String getErrandById(String errandId){
 
+        WebClient webClient = WebClient.builder()
+                .baseUrl(caseDataErrandsUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
 
 
-        return null;
+        JsonNode jsonNode = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/errands")
+                        .queryParam("caseType", "PARKING_PERMIT")
+                        .queryParam("id", errandId)
+                        .build())
+                .retrieve()
+                .bodyToMono(JsonNode.class)
+                .block();
+
+        System.out.println(jsonNode.toPrettyString());
+
+        return jsonNode.toPrettyString();
     }
 
 }
