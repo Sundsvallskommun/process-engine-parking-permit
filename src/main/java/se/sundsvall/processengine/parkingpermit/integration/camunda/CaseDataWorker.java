daaea859c6a1f7aa2395c4b452bb9cdd9fc61519
mattias.gradin@apendo.se <mattias.gradin@apendo.se>
2022-06-15 13:50:45 +0200

    First version of Get request to CaseData

--- src/main/java/se/sundsvall/processengine/parkingpermit/integration/camunda/CaseDataWorker.java
+++ src/main/java/se/sundsvall/processengine/parkingpermit/integration/camunda/CaseDataWorker.java
@@ -5,15 +5,27 @@ import org.camunda.bpm.client.task.ExternalTask;
 import org.camunda.bpm.client.task.ExternalTaskHandler;
 import org.camunda.bpm.client.task.ExternalTaskService;
 import org.springframework.context.annotation.Configuration;
+import se.sundsvall.processengine.parkingpermit.integration.casedata.ErrandsClient;
 
 @Configuration
 @ExternalTaskSubscription("caseData")
 public class CaseDataWorker implements ExternalTaskHandler {
 
+    private ErrandsClient errandsClient;
+
+    public CaseDataWorker(ErrandsClient errandsClient){
+        this.errandsClient = errandsClient;
+    }
+
     @Override
     public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
 
-        System.out.println("Test test :)");
+        String caseId = externalTask.getVariable("caseId");
+
+        String errandString = errandsClient.getErrandById(caseId);
+
+        System.out.println(errandString);
+        
         externalTaskService.complete(externalTask);
     }
 }
