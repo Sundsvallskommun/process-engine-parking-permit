39fd8e52cf53889f470a7b48b6f459ea649d841a
mattias.gradin@apendo.se <mattias.gradin@apendo.se>
2022-07-22 16:46:42 +0200

    Added more implementation to Enum class

--- src/main/java/se/sundsvall/processengine/parkingpermit/integration/casedata/enums/DecisionType.java
+++ src/main/java/se/sundsvall/processengine/parkingpermit/integration/casedata/enums/DecisionType.java
@@ -1,5 +1,11 @@
 package se.sundsvall.processengine.parkingpermit.integration.casedata.enums;
 
 public enum DecisionType {
-    APPROVAL, REJECTION
+    APPROVAL("APPROVAL"), REJECTION("REJECTION ");
+
+    private String decsionText;
+
+    DecisionType(String decsionText){
+        this.decsionText = decsionText;
+    }
 }
